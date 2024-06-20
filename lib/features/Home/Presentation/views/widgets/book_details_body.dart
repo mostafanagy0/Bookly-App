9f0dcd90ad7c9ebe649c8d9dd740ed4c7f055ee0
mostafanagy0 <mostafanagy855@gmail.com>
2024-06-20 04:46:40 -0700

    custom Book Details Appbar

--- lib/features/Home/Presentation/views/widgets/book_details_body.dart
+++ lib/features/Home/Presentation/views/widgets/book_details_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/Home/Presentation/views/widgets/custom_Appbar_book_details.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -5,6 +6,13 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          CustomAppbarBookDetails(),
+        ],
+      ),
+    );
   }
 }
