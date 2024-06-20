f679b80a7d2392b3fb5c063a0fe53b473ca449f4
mostafanagy0 <mostafanagy855@gmail.com>
2024-06-20 05:24:13 -0700

    Add titel & Subtitel  T boo Details

--- lib/features/Home/Presentation/views/widgets/book_details_body.dart
+++ lib/features/Home/Presentation/views/widgets/book_details_body.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/Home/Presentation/views/widgets/Custom_book_Item.dart';
 import 'package:bookly_app/features/Home/Presentation/views/widgets/custom_Appbar_book_details.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -14,8 +16,28 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomAppbarBookDetails(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: Width * .17),
+            padding: EdgeInsets.symmetric(horizontal: Width * .2),
             child: const CustomBookItem(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'rudyard Kipling',
+              style: Styles.textStyl18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ],
       ),
