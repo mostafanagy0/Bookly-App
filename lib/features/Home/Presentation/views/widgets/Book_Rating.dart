f9199785bd63d0ac4f1abc3813206f3ab4f910c1
mostafanagy0 <mostafanagy855@gmail.com>
2024-06-20 05:49:06 -0700

    Add Rating Book To Book Details

--- lib/features/Home/Presentation/views/widgets/Book_Rating.dart
+++ lib/features/Home/Presentation/views/widgets/Book_Rating.dart
@@ -1,16 +1,20 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
+          size: 16,
           color: Color(0xffFFDD4F),
         ),
         const SizedBox(
@@ -23,9 +27,12 @@ class BookRating extends StatelessWidget {
         const SizedBox(
           width: 5,
         ),
-        Text(
-          '(465)',
-          style: Styles.textStyle14.copyWith(color: const Color(0xff707070)),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(465)',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
         )
       ],
     );
--- lib/features/Home/Presentation/views/widgets/book_details_body.dart
+++ lib/features/Home/Presentation/views/widgets/book_details_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/Home/Presentation/views/widgets/Book_Rating.dart';
 import 'package:bookly_app/features/Home/Presentation/views/widgets/Custom_book_Item.dart';
 import 'package:bookly_app/features/Home/Presentation/views/widgets/custom_Appbar_book_details.dart';
 import 'package:flutter/material.dart';
@@ -38,6 +39,12 @@ class BookDetailsViewBody extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
