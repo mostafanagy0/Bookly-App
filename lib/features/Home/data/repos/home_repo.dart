a73efdc8c46019e6437b521895acfe7da502fdd4
mostafanagy0 <mostafanagy855@gmail.com>
2024-06-25 09:07:01 -0700

    Create Home Repo Method & Error File &use dartz packege

--- lib/features/Home/data/repos/home_repo.dart
+++ lib/features/Home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class RepoHome {
-  fetchFeatureBooks();
-  fetchBestSelerBook();
+  //    <Lift side , Right Side>
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSelerBook();
 }
